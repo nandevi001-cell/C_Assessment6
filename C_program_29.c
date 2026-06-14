97dac536c54a84ad5d135bdefef6223210e59c53
nandevi001-cell <nandevi001@gmail.com>
2026-06-14 23:12:59 +0530

    Create C_program_29.c

---
+++ C_program_29.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main()
+{
+    int a, b, i;
+
+    printf("Enter two numbers: ");
+    scanf("%d%d", &a, &b);
+
+    if(a > b)
+    {
+        i = a;
+    }
+    else
+    {
+        i = b;
+    }
+
+    while(1)
+    {
+        if(i % a == 0 && i % b == 0)
+        {
+            printf("LCM = %d", i);
+            break;
+        }
+
+        i = i + 1;
+    }
+
+    return 0;
+}
